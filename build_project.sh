b7e187a18f47fce72eca4915abcd5b26e22454f5
Farid Alijani <farid.alijani@gmail.com>
2018-11-14 13:39:48 +0200

    bash modified

--- build_project.sh
+++ build_project.sh
@@ -9,25 +9,25 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd "$BASEDIR/Thirdparty/DBoW2"
 rm -rf build/ lib/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
 echo "Configuring and building Thirdparty/g2o ..."
 cd "$BASEDIR/Thirdparty/g2o"
 rm -rf build/ lib/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
 echo "-----------------------Uncompressing vocabulary-----------------------"
 cd "$BASEDIR/Vocabulary"
 rm -rf ORBvoc.txt
 
-#tar -xf ORBvoc.txt.tar.gz
+tar -xf ORBvoc.txt.tar.gz
 
 echo "-----------------------Configuring & building ORB_SLAM2-----------------------"
 
@@ -43,17 +43,17 @@ rm -rf stereo_kitti stereo_euroc
 cd "$BASEDIR"
 rm -rf build/ lib/
 
-#mkdir build
-#cd build
-#cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
 
 echo "-----------------------Compiling ORB_SLAM2 >>>(ROS)<<< nodes-----------------------"
 
 cd "$BASEDIR/Examples/ROS/ORB_SLAM2"
 rm -rf cameraPub Mono MonoAR RGBD Stereo build/ lib/
 
-#mkdir build
-#cd build
-#cmake -DROS_BUILD_TYPE=$BUILD_TYPE ..
-#make -j$NUM_PROC
+mkdir build
+cd build
+cmake -DROS_BUILD_TYPE=$BUILD_TYPE ..
+make -j$NUM_PROC
